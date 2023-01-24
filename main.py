ba88f076e694557713d7cbad390cbfc9cf1b3e57
narumol01 <63070094@kmitl.ac.th>
2023-01-24 14:54:36 +0700

    test2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def test(name, lastname):
     print("Hello", name, lastname, sep=" ")
+    print("It's me I wonder you can call me", name+"02", "and say goodbye", sep="  ", end="<3")
 
 test("pao", "halo")
